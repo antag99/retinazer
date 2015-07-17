705376250bdc9762c5925b1f1af2ddab62961dfe
Anton Gustafsson <antag99@gmail.com>
2015-07-17 14:23:29 +0200

    Remove Set implementation from EntitySet

--- retinazer/src/main/java/com/github/antag99/retinazer/EntitySet.java
+++ retinazer/src/main/java/com/github/antag99/retinazer/EntitySet.java
@@ -1,22 +1,11 @@
 package com.github.antag99.retinazer;
 
-import java.util.AbstractSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import com.github.antag99.retinazer.utils.Mask;
 
-/**
- * Entity set; supports retrieving and destroying entities, but does
- * not support insertion; use {@link Engine#createEntity()} for that.
- * </p>
- * A set of entities is updated as entities are added to the engine; all sets
- * target a specific kind of entity matched by a {@link Family}. To get an entity set,
- * use {@link Engine#getEntities()} or {@link Engine#getEntitiesFor(FamilyConfig)}.
- * </p>
- * To process all entities in a set, the {@link #process(EntityProcessor)} method can be used.
- */
-public final class EntitySet extends AbstractSet<Entity> {
+public final class EntitySet implements Iterable<Entity> {
     // Engine the entities belong to
     final Engine engine;
     // Mask updated by FamilyManager
@@ -58,37 +47,8 @@ public final class EntitySet extends AbstractSet<Entity> {
     }
 
     @Override
-    public boolean contains(Object o) {
-        if (!(o instanceof Entity))
-            return false;
-        Entity entity = (Entity) o;
-        if (entity.getEngine() != engine)
-            return false;
-        return entities.get(entity.getIndex());
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        if (!(o instanceof Entity))
-            return false;
-        Entity entity = (Entity) o;
-        if (entity.getEngine() != engine)
-            return false;
-        boolean removed;
-        if (removed = entities.get(entity.getIndex())) {
-            entity.destroy();
-        }
-        return removed;
-    }
-
-    @Override
     public Iterator<Entity> iterator() {
         return new EntityIterator();
-    }
-
-    @Override
-    public int size() {
-        return entities.cardinality();
     }
 
     public void process(EntityProcessor processor) {
