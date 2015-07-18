7803c0c362164d0c4cbb54020b0e6f9a004bd223
Anton Gustafsson <antag99@gmail.com>
2015-07-18 10:37:22 +0200

    Switch to maven-release-plugin for releases, drop snapshots

--- deploy.sh
+++ deploy.sh
@@ -1,61 +1,37 @@
 #!/bin/bash
+# This handy script is run by Travis CI; generates badges and other stuff
 
-if ! [ "$TRAVIS" ]; then
-    echo "Deployment script should be run in a Travis environment"
+if ! [ "$GIT_NAME" && "$GIT_EMAIL" && "$GH_TOKEN" ]; then
+    echo "This script is intended to be run by Travis CI"
     exit 1
 fi
 
-REMOTE=`git config remote.origin.url`
-git remote set-url --push origin ${REMOTE/#git:/https:}
-git remote set-branches --add origin gh-pages
-git fetch
-
-## Count lines of code
-LINES_OF_CODE=`git ls-files | grep .java$ | xargs cat | wc -l`
-
-## Deploy artifacts to ~/maven
-rm -rfv ~/maven
-mvn deploy
-
-## Find current version to display in commit message
-VERSION=$(mvn -q \
-    -Dexec.executable="echo" \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-SNAPSHOT=`echo ${VERSION} | grep -- -SNAPSHOT`
 REVISION=`git rev-parse HEAD`
 
-## Discard files changed locally
-git reset --hard
-git clean -dffx .
-
-## Checkout gh-pages branch and copy the artifacts
-git checkout gh-pages
-cp -rf ~/maven .
-
-if [ $SNAPSHOT ]
-then
-    # Use commit hash in the message
-    VERSION=$REVISION
-else
-    # Remove old snapshot versions
-    rm -rfv ./maven/**/*/*-SNAPSHOT/
-fi
-
-## Add lines of code badge
-wget https://img.shields.io/badge/lines_of_code-${LINES_OF_CODE}-orange.svg -O loc.svg
-
-## Commit changes
 git config user.name ${GIT_NAME}
 git config user.email ${GIT_EMAIL}
 git config credential.helper "store --file=.git/credentials"
 printf "%s" "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-git add --all
-git commit -m "Upload artifacts for ${VERSION}"
+## Count lines of code
+LINES_OF_CODE=`git ls-files | grep .java$ | xargs cat | wc -l`
+
+# git reset --hard
+# git clean -dffx .
+
+git checkout gh-pages
+cp -rf ~/maven .
+
+## Update lines of code
+if [ "$LINES_OF_CODE" != "$OLD_LINES_OF_CODE" ]; then
+    echo "$LINES_OF_CODE" > loc.txt
+    wget https://img.shields.io/badge/lines_of_code-${LINES_OF_CODE}-orange.svg -O loc.svg
+    git add loc.txt
+    git add loc.svg
+    git commit -m "Update lines of code for $REVISION"
+fi
+
 git push origin gh-pages
 rm .git/credentials
 
-## Switch back to master branch
 git checkout master
