4da8770c8102c0bf748c2a447a692524fac2e746
Anton Gustafsson <antag99@gmail.com>
2015-08-02 12:34:45 +0200

    Introduce Experimental annotation

---
+++ retinazer/src/main/java/com/github/antag99/retinazer/utils/Experimental.java
@@ -0,0 +1,14 @@
+package com.github.antag99.retinazer.utils;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Informal annotation used to denote that a feature is experimental and may be
+ * removed in a future version.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Documented
+public @interface Experimental {
+}
