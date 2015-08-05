4969b489493ec4e9f87ab24412f4bb5a96dd1693
Anton Gustafsson <antag99@gmail.com>
2015-08-05 17:29:17 +0200

    Fix issue where artemis-odb systems were never weaved in benchmarks

--- benchmarks.sh
+++ benchmarks.sh
@@ -22,31 +22,31 @@ java -jar retinazer-benchmarks/target/microbenchmarks.jar .* \
   tee retinazer-benchmarks/target/results-plain.log
 
 # Run artemis-odb benchmarks with various weaving configurations
-mvn install -DskipTests=true -Dartemis.optimizeSystems=true | \
+mvn install -DskipTests=true -Dartemis.optimizeEntitySystems=true | \
   tee retinazer-benchmarks/target/build-plain-fast.log
 java -jar retinazer-benchmarks/target/microbenchmarks.jar .*\\.artemis\\..* \
   -rf json -rff retinazer-benchmarks/target/results-plain-fast.json $PARAMS | \
   tee retinazer-benchmarks/target/results-plain-fast.log
 
-mvn install -DskipTests=true -Dartemis.optimizeSystems=false -Dartemis.enablePooledWeaving=true | \
+mvn install -DskipTests=true -Dartemis.optimizeEntitySystems=false -Dartemis.enablePooledWeaving=true | \
   tee retinazer-benchmarks/target/build-pooled.log
 java -jar retinazer-benchmarks/target/microbenchmarks.jar .*\\.artemis\\..* \
   -rf json -rff retinazer-benchmarks/target/results-pooled.json $PARAMS | \
   tee retinazer-benchmarks/target/results-pooled.log
 
-mvn install -DskipTests=true -Dartemis.optimizeSystems=false -Dartemis.enablePackedWeaving=true | \
+mvn install -DskipTests=true -Dartemis.optimizeEntitySystems=false -Dartemis.enablePackedWeaving=true | \
   tee retinazer-benchmarks/target/build-packed.log
 java -jar retinazer-benchmarks/target/microbenchmarks.jar .*\\.artemis\\..* \
   -rf json -rff retinazer-benchmarks/target/results-packed.json $PARAMS | \
   tee retinazer-benchmarks/target/results-packed.log
 
-mvn install -DskipTests=true -Dartemis.optimizeSystems=true -Dartemis.enablePooledWeaving=true | \
+mvn install -DskipTests=true -Dartemis.optimizeEntitySystems=true -Dartemis.enablePooledWeaving=true | \
   tee retinazer-benchmarks/target/build-pooled-fast.log
 java -jar retinazer-benchmarks/target/microbenchmarks.jar .*\\.artemis\\..* \
   -rf json -rff retinazer-benchmarks/target/results-pooled-fast.json $PARAMS | \
   tee retinazer-benchmarks/target/results-pooled-fast.log
 
-mvn install -DskipTests=true -Dartemis.optimizeSystems=true -Dartemis.enablePackedWeaving=true | \
+mvn install -DskipTests=true -Dartemis.optimizeEntitySystems=true -Dartemis.enablePackedWeaving=true | \
   tee retinazer-benchmarks/target/build-packed-fast.log
 java -jar retinazer-benchmarks/target/microbenchmarks.jar .*\\.artemis\\..* \
   -rf json -rff retinazer-benchmarks/target/results-packed-fast.json $PARAMS | \
