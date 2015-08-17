e0ce0b245780c1f90965f610b0a0303246b591ab
Anton Gustafsson <antag99@gmail.com>
2015-08-17 19:14:09 +0200

    Implement unit test for EntityProcessorSystem

---
+++ retinazer/src/test/java/com/github/antag99/retinazer/EntityProcessorSystemTest.java
@@ -0,0 +1,53 @@
+package com.github.antag99.retinazer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.github.antag99.retinazer.utils.Mask;
+
+public class EntityProcessorSystemTest {
+    public static final class TestEntityProcessorSystem extends EntityProcessorSystem {
+        public EntitySet processedEntities = new EntitySet();
+
+        public TestEntityProcessorSystem() {
+            super(Family.create());
+        }
+
+        @Override
+        protected void process(int entity) {
+            if (processedEntities.contains(entity))
+                throw new IllegalStateException("entity already processed: " + entity);
+            processedEntities.addEntity(entity);
+        }
+    }
+
+    @Test
+    public void testEntityProcessorSystem() {
+        TestEntityProcessorSystem system = new TestEntityProcessorSystem();
+        Engine engine = EngineConfig.create().withSystem(system).finish();
+        Mask entities = new Mask();
+        int a, b, c;
+        entities.set(a = engine.createEntity().getEntity());
+        entities.set(b = engine.createEntity().getEntity());
+        entities.set(c = engine.createEntity().getEntity());
+        engine.update();
+        assertEquals(system.processedEntities.getMask(), entities);
+        system.processedEntities.clear();
+        engine.destroyEntity(b);
+        entities.clear(b);
+        engine.update();
+        assertEquals(system.processedEntities.getMask(), entities);
+        system.processedEntities.clear();
+        entities.set(b = engine.createEntity().getEntity());
+        engine.update();
+        assertEquals(system.processedEntities.getMask(), entities);
+        system.processedEntities.clear();
+        engine.destroyEntity(a);
+        engine.destroyEntity(b);
+        engine.destroyEntity(c);
+        entities.clear(a);
+        entities.clear(b);
+        entities.clear(c);
+    }
+}
